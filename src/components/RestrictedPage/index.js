ff7a2a76267e1b9ebecec8fb21fdad9b8f84083e
glen1986 <escobar.glen@gmail.com>
2021-09-12 13:42:38 -0300

    conditional

--- src/components/RestrictedPage/index.js
+++ src/components/RestrictedPage/index.js
@@ -1,20 +1,20 @@
 import './style.css' 
 const RestrictedPage = ({isLoggedIn, user, Login, Logout}) => {
-   if(isLoggedIn === false){
- return(
-   <div>
-      <h1>conectate primero, {user}</h1>
-      <button onClick = {Login}>Entrar</button>
-   </div>
-     )
-  }else{
-     return(
-     <div>
-        <h1>Hola, {user}</h1>
+   return(
+<div>
+{isLoggedIn ?( 
+  <div>
+      <h1>Hola, {user}</h1>
         <p>bienvenido</p>
         <button onClick = {Logout}>Sair</button>
-     </div>
-     )
-  }
+  </div>
+  ):(
+  <div>
+    <h1>conectate primero, {user}</h1>
+    <button onClick = {Login}>Entrar</button>     
+  </div>
+)}
+</div>
+   )
 }   
 export default RestrictedPage;
